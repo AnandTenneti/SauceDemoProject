7ba09dcd1eafc8aa8559e92321ebc3cf3886fee6
AnnadTenneti <tenneti.anand@gmail.com>
2026-06-25 14:06:35 +0530

    Bulk update of pages with minor changes

--- utils/webdriver_utils.py
+++ utils/webdriver_utils.py
@@ -1,21 +1,22 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from config.config import settings
 
 
 class WebDriverUtils:
     @staticmethod
     def wait_until_visible(driver, locator):
 
-        return WebDriverWait(driver, 10).until(EC.visibility_of_element_located(locator))
+        return WebDriverWait(driver, settings["timeout"]).until(EC.visibility_of_element_located(locator))
 
     @staticmethod
     def wait_until_elements_visible(driver, locator):
-        return WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located(locator))
+        return WebDriverWait(driver, settings["timeout"]).until(EC.visibility_of_all_elements_located(locator))
 
     @staticmethod
     def wait_until_clickable(driver, locator):
-        return WebDriverWait(driver, 10).until(EC.element_to_be_clickable(locator))
+        return WebDriverWait(driver, settings["timeout"]).until(EC.element_to_be_clickable(locator))
 
     @staticmethod
     def wait_until(driver, condition, timeout=10):
-        return WebDriverWait(driver, timeout).until(condition)
+        return WebDriverWait(driver, settings["timeout"]).until(condition)
